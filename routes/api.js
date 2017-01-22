e5b21a1d335cb6a68df6d85ae9253bdc983f93c4
Davide Vitelaru <davide@davidevitelaru.com>
2017-01-22 17:06:48 +0000

    Core API Completed

--- routes/api.js
+++ routes/api.js
@@ -169,15 +169,86 @@ router.post('/session/vote/submit', function(req, res, next) {
     if (req.body.session_id === undefined) {
       return res.status(400).json({"status": "error", "message": "'session_id' has not been set"});
     }
+    if (req.body.ticket_index === undefined) {
+      return res.status(400).json({"status": "error", "message": "'ticket_index' has not been set"});
+    }
+    if (req.body.vote === undefined) {
+      return res.status(400).json({"status": "error", "message": "'vote' has not been set"});
+    }
+    if (req.body.username === undefined) {
+      return res.status(400).json({"status": "error", "message": "'username' has not been set"});
+    }
+
+
+    var mongo = require('mongodb');
+    var sessionid = new mongo.ObjectID(req.body.session_id);
+    var ticket_index = parseInt(req.body.ticket_index);
+    var vote = parseInt(req.body.vote);
+    var username = req.body.username;
+
+    // Checking if voting is enabled and if current index matches the
+    // one the vote was submitted for
+    var collection = req.db.get('sessions');
+
+    collection.find({
+      _id: sessionid,
+      current_index: ticket_index
+    }, function(err, doc) {
+      if (err) {
+        return res.status(500).json({"status": "error", "message": "There was a problem submitting the vote for the current ticket"});
+      }
+      else {
+        if (doc.length === 0) {
+          return res.status(400).json({"status": "error", "message": "'ticket_index' might not match `current_index`."});
+        }
+        else if (doc[0].voting === false) {
+          return res.status(409).json({"status": "error", "message": "Cannot submit vote if voting has not been started"});
+        }
+        else {
+          collection.update({
+              _id: sessionid,
+              current_index: ticket_index
+          },{
+            $push: {
+              votes: {
+                username: username,
+                vote: vote
+              }
+            }
+          }, function(err, doc) {
+            if (err) {
+              return res.status(500).json({"status": "error", "message": "There was a problem submitting the vote for the current ticket"});
+            }
+            else {
+              return res.json({"status": "ok"});
+            }
+          });
+        } // else
+      } // else
+    }); // collection.find
 
 });
 
-router.get('/session/refresh', function(req, res, next) {
+router.get('/session/refresh/:session', function(req, res, next) {
 
-    if (req.body.session_id === undefined) {
+    if (req.params.session === undefined) {
       return res.status(400).json({"status": "error", "message": "'session_id' has not been set"});
     }
 
+    var mongo = require('mongodb');
+    var sessionid = new mongo.ObjectID(req.params.session);
+    var collection = req.db.get('sessions');
+
+    collection.find({
+      _id: sessionid,
+    }, function(err, doc) {
+      if (err) {
+        return res.status(500).json({"status": "error", "message": "Could not refresh session status"});
+      }
+      else {
+        return res.json(doc);
+      }
+    });
 });
 
 
